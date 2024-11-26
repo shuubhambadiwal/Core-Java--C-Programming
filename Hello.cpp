cf5fa6b8c18327d5c46998dd1e320bbbda205f4c
badiwalshubham <shubhambadiwal2@gmail.com>
2024-11-26 19:28:04 +0530

    Hello program

---
+++ Hello.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+// Main function: entry point for execution
+int main() {
+
+    // writing print statement to print hello world
+    printf("Hello Worl");
+
+    return 0;
+}
