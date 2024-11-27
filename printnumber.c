d577db63b915bfdeccc9038de6b7ad447d8f1138
shuubhambadiwal <shubhambadiwal2@gmail.com>
2024-11-27 14:06:00 +0530

    Print numbers program

---
+++ printnumber.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+    int num1 = 76;
+    printf("%d", num1);
+    return 0;
+}
---
+++ printnumbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int num1 = -23;
+    int num2 = 678;
+    int num3 = -4;
+    int num4 = 21;
+    
+    printf("%d", num1);
+    printf(", %d", num2);
+    printf(", %d", num3);
+    printf(", %d", num4);
+    
+    return 0;
+}
