a277355727f04f6490f9af9fcba63b64bc4d017a
shuubhambadiwal <shubhambadiwal2@gmail.com>
2024-11-27 14:32:10 +0530

    operators programs all in one

---
+++ operators9.c
@@ -0,0 +1,13 @@
+//  23+89+12+56
+#include <stdio.h>
+int main()
+{
+    int num1 = 23;
+    int num2 = 89;
+    int num3 = 12;
+    int num4 = 56;
+    int res = num1 + num2 + num3 + num4;
+    printf("%d", res);
+
+    return 0;
+}
