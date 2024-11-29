f1c8b0f0f05b26bcc9b0ffc88e73756e306e175e
shuubhambadiwal <shubhambadiwal2@gmail.com>
2024-11-29 17:40:33 +0530

    100 to 1 print numbers

--- nestedif.c/56to784.c
+++ nestedif.c/56to784.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 
 int main(){
-    int num = 55;
-    while( num < 784){
-        num++;
+    int num = 101;
+    while( num > 1){
+        num--;
         printf("%d\n", num);
     }
     return 0;
