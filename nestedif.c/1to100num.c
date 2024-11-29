0fbb1f784d0196976600f0bc7158f3ff94514efb
shuubhambadiwal <shubhambadiwal2@gmail.com>
2024-11-29 17:39:11 +0530

    1 to 100 number print

---
+++ nestedif.c/1to100num.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+    int num = 0;
+    while(num<100){
+         num ++;
+        printf("%d\n", num);
+    }
+    return 0;
+}
---
+++ nestedif.c/56to784.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+    int num = 55;
+    while( num < 784){
+        num++;
+        printf("%d\n", num);
+    }
+    return 0;
+}
