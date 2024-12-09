3a5237d5551ec8bc217cbf8f517a4987c10d6916
shuubhambadiwal <shubhambadiwal2@gmail.com>
2024-12-09 15:51:32 +0530

    Odd num program using C

---
+++ for_loops & while/oddnum.c
@@ -0,0 +1,17 @@
+// number :odd digit 
+// input :3653542 
+
+#include<stdio.h>
+
+int main() {
+    int i;
+    for (i=2; i<=6; i++){
+        if (i%2 != 0){
+            printf("%d", i);
+        }
+    }
+    return 0;
+}
+
+
+
