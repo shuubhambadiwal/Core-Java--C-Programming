f0b9a69812d02dd8acdc8f15eb9257c5d9aedb2e
shuubhambadiwal <shubhambadiwal2@gmail.com>
2024-12-12 02:18:12 +0530

    SUm of multiple nums in java

---
+++ Opr8.java
@@ -0,0 +1,8 @@
+// 23+89+12+56
+public class Opr8 {
+    public static void main(String[] args) {
+        int a = 23, b = 89, c = 12, d = 56, e;
+        e = a + b + c + d;
+        System.out.println("The value of the expsn is " + e);
+    }
+}
