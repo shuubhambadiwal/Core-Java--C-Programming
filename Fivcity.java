eac0b98ff71bd23a0d5448aa7b6edcf7ed2d67bc
shuubhambadiwal <shubhambadiwal2@gmail.com>
2024-12-12 01:02:07 +0530

    Fav city name in new line

---
+++ Fivcity.java
@@ -0,0 +1,5 @@
+public class Fivcity {
+    public static void main(String[] args) {
+        System.out.println("New York" + '\n' + "Jaipur" + '\n' + "Bengluru" + '\n' + "Mathura" + '\n' + "Rishikesh");
+    }
+}
