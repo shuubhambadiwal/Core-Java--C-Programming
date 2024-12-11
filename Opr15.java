88bc91d4f2416977e2261d8cee8419300a575787
shuubhambadiwal <shubhambadiwal2@gmail.com>
2024-12-12 02:50:37 +0530

    Oprtatr mix in java

---
+++ Opr15.java
@@ -0,0 +1,10 @@
+// 8.9*1.67-(6.21-5.6)+7.8-34 +157
+
+public class Opr15 {
+    public static void main(String[] args) {
+        float a = 8.9f, b = 1.67f, c = 6.21f, d = 5.6f, e = 7.8f, v;
+        int f = 34 , g = 157; 
+        v = a*b-(c-d)+e-f+g;
+        System.out.println("The Result of this expn is " + v);
+    }
+}
