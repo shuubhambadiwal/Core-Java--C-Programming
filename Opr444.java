7b1cee6b8281f4a2586e959e9044cb56326a2fde
shuubhambadiwal <shubhambadiwal2@gmail.com>
2024-12-12 03:19:59 +0530

    Divide using user input in java

--- Opr444.java
+++ Opr444.java
@@ -1,3 +1,13 @@
+import java.util.Scanner;
+
 public class Opr444 {
-    
+    public static void main(String[] args) {
+        int a,b,c;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Values ");
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = a/b;
+        System.out.println("The value of A divide by B is " + c);
+    }
 }
