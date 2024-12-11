073f29aaeff568244fbaa3ed89350e4e11ba2161
shuubhambadiwal <shubhambadiwal2@gmail.com>
2024-12-12 02:46:32 +0530

    Operator program mix in java

---
+++ Opr14.java
@@ -0,0 +1,9 @@
+// 3.4+1.2*(3.7-1.2)/2+6.8
+public class Opr14 {
+    public static void main(String[] args) {
+        float a = 3.4f, b = 1.2f, c = 3.7f, d = 1.2f, f = 6.8f,v;
+        int e = 2;
+        v = a + b * ( c - d ) / e + f;
+        System.out.println("The Result of the expn is " + v);
+    }
+}
