02867a55c063e11fb1026b67499208d89f90804d
shuubhambadiwal <shubhambadiwal2@gmail.com>
2024-12-14 15:10:38 +0530

    Temprature convert code java Test Q

---
+++ TicketReserveation.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class TicketReserveation {
+    public static void main(String[] args) {
+        System.out.println("Enter the Age ");
+        Scanner sc = new Scanner(System.in);
+    }
+}
