649260d9f5f6bfa90a023c1e568f999fab2186af
shuubhambadiwal <shubhambadiwal2@gmail.com>
2024-12-14 13:21:18 +0530

    Updated the old code

---
+++ Evenodd.java
@@ -0,0 +1,10 @@
+// 5)
+// Even or Odd:
+// Write a Java program to check if a given number is even or odd.
+
+public class Evenodd {
+    public static void main(String[] args) {
+        
+        System.out.println();
+    }
+}
--- Userinput.java
+++ Userinput.java
@@ -4,12 +4,17 @@
 // Declare an integer variable named age and initialize it with the value 25.
 // Declare a double variable named price and initialize it with the value 19.99.
 
+import java.util.Scanner;
+
 public class Userinput {
     public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
        int aged;
-       aged = 25;
        double price;
-       price = 19.99d;
+       System.out.print("Enter the value of Age");
+       aged = sc.nextInt();
+       System.out.print("Enter the value of price");
+       price = sc.nextInt();
        System.out.println("Age " + aged);
        System.out.println("Price " + price);
     }
