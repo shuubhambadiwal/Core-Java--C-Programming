4eb9e38ba17eb7c67b085f5b61b48954db93c268
shuubhambadiwal <shubhambadiwal2@gmail.com>
2024-12-14 13:23:33 +0530

    Upadteed the codes with user input

--- Userinput2.java
+++ Userinput2.java
@@ -4,11 +4,16 @@
 // Create two variables, num1 and num2, with values 10 and 5 respectively. 
 // Perform addition, subtraction, multiplication, and division operations on them.
 
+import java.util.Scanner;
+
 public class Userinput2 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         int num1, num2, sum, sub, mul, div;
-        num1= 10;
-        num2= 5;
+        System.out.print("The number 1 value is ");
+        num1= sc.nextInt();
+        System.out.print("The number 2 value is ");
+        num2= sc.nextInt();
         // Addition
         sum = num1 + num2;
         // Subtraction
