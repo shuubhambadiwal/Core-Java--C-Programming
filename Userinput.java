39aa47a89fd57f0403e856ada02eb9c5273b792f
shuubhambadiwal <shubhambadiwal2@gmail.com>
2024-12-14 12:55:32 +0530

    Test question 1 on sat

---
+++ Userinput.java
@@ -0,0 +1,10 @@
+public class Userinput {
+    public static void main(String[] args) {
+       int aged;
+       aged = 25;
+       double price;
+       price = 19.99d;
+       System.out.println("Age " + aged);
+       System.out.println("Price " + price);
+    }
+}
