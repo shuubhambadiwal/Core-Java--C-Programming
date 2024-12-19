8946569e5bcb7809a719180477e86fd1f51ca319
Shuubham Badiwal <shubhambadiwal2@gmail.com>
2024-12-19 17:00:15 +0530

    Add files via upload

---
+++ src/Java/Basic/Favfood.java
@@ -0,0 +1,5 @@
+public class Favfood {
+    public static void main (String[] args) {
+        System.out.println("Dosa, Dhokla, Pizza, White, Pasta, Achar-Batti");
+    }
+}
---
+++ src/Java/Basic/Fivefrn.java
@@ -0,0 +1,5 @@
+public class Fivefrn {
+    public static void main(String[] args) {
+        System.out.println("Ram, Shree, Brhma, Shesh");
+    }
+}
