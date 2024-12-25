868d4f7c36a57316f1e41d1733ff7de749a08801
shuubhambadiwal <shubhambadiwal2@gmail.com>
2024-12-25 14:07:08 +0530

    update

--- C/user Input/userinpitmul5.c
+++ C/user Input/userinpitmul5.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 int main()
-{
+{   
+    char c;
     int num1;
-    int num2;
-    printf("Enter the first number");
+    float num2;
+    
+    printf("Enter the char ");
+    scanf("%c", &c);
+
+    printf("Enter the first number ");
     scanf("%d", &num1);
-    printf("Enter the second number");
-    scanf("%d", &num2);
-    int res = num1 * num2;
-    printf("The sum of two number is %d", res);
+
+    printf("Enter the second number ");
+    scanf("%f", &num2);
+
+    printf("%d", num1);
+    printf("%f", num2);
+    printf("%c", c);
+
     return 0;
 }
